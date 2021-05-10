032de635ed938246fb024fc05ca03c1230c5b8bd
cmtilo <zzz2501033@hotmail.com>
2021-05-10 22:26:05 +0800

    'fixed'

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -2,8 +2,8 @@ const request = require('request')
 
 request.get('https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (error, response, body) => {
-    const json = JSON.parse(body) // 將body轉JSON格式物件資料
     try {
+      const json = JSON.parse(body) // 將body轉JSON格式物件資料
       for (let i = 0; i < json.length; i++) {
         console.log(`${json[i].id} ${json[i].name}`)
       } // 取出每本序號及書名
