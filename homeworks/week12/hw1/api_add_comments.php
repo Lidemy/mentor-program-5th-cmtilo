881c927e07bd448748df1749cd1ce2391c8c225a
cmtilo <zzz2501033@hotmail.com>
2021-07-19 15:45:32 +0800

    hw

---
+++ homeworks/week12/hw1/api_add_comments.php
@@ -0,0 +1,42 @@
+<?php
+  require_once('conn.php');
+  header('Content-type:application/json;charset=utf-8');
+  header('Access-Control-Allow-Origin: *');
+  
+  if (empty($_POST['site_key']) ||
+      empty($_POST['nickname']) ||
+      empty($_POST['content'])) {
+    $json = array(
+      'ok' => false,
+      'message' => 'Please input missing fields'
+    );
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+  
+  $site_key = $_POST['site_key'];
+  $nickname = $_POST['nickname'];
+  $content = $_POST['content'];
+
+  $sql = "INSERT INTO cmtilo_discussions(site_key, nickname, content) VALUES (?, ?, ?)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('sss', $site_key, $nickname, $content);
+  $result = $stmt->execute();
+  if(!$result) {
+    $json = array(
+      'ok' => false,
+      'message' => $conn->error
+    );
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+
+  $json = array(
+    'ok' => true,
+    'message' => 'success'
+  );
+  $response = json_encode($json);
+  echo $response;
+?>
